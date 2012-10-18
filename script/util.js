4bf6bd02efad3ec892837dc01979907e6bbad8ae
gotomanners <manners@gotomanners.com>
2012-10-18 18:08:05 +0100

    Added util module

--- script/util.js
+++ script/util.js
@@ -1,5 +1,34 @@
-/**
- * User: manners
- * Date: 18/10/2012
- * Time: 17:52
- */
+var Util = Util || {};
+
+
+Util.loadResource = function(window, filename, filetype) {
+	var loadedResource = undefined;
+	if (filetype === "js") { //if filename is a external JavaScript file
+		loadedResource = window.document.createElement('script');
+		loadedResource.setAttribute("type", "text/javascript");
+		loadedResource.setAttribute("src", filename);
+	}
+	else if (filetype === "css") { //if filename is an external CSS file
+		loadedResource = window.document.createElement("link");
+		loadedResource.setAttribute("rel", "stylesheet");
+		loadedResource.setAttribute("type", "text/css");
+		loadedResource.setAttribute("href", filename);
+	}
+	if (typeof loadedResource !== undefined) {
+		window.document.getElementsByTagName("head")[0].appendChild(loadedResource)
+	}
+};
+
+Util.printDiv = function(options, callback) {
+	var printWindowProperties = "toolbar=no, location=no, directories=no, status=no, " +
+			"menubar=yes, scrollbars=yes, resizable=yes, width=" + options.width + ", height=" + options.height;
+	var printWindow = window.open("about:blank", "Print Order", printWindowProperties);
+	printWindow.document.write("<div id='printDivBody'>This is a dummy div</div>");
+
+	Util.loadResource(printWindow, "../bootstrap/css/bootstrap.css", "css");
+	Util.loadResource(printWindow, "../foundation/stylesheets/foundation.css", "css");
+	Util.loadResource(printWindow, "../css/print.css", "css");
+	Util.loadResource(printWindow, "../css/general.css", "css");
+
+	callback(printWindow, "printDivBody");
+};
